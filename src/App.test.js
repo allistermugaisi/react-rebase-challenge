d9a245dd26d927468cf6573272194149e9408ae7
allistermugaisi <allistermugaisi@gmail.com>
2021-07-26 01:14:14 +0300

    fixed rebase conflicts

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,19 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders My really cool blog site logo', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/My really cool blog site/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/My really cool blog site/i);
+	expect(linkElement).toBeInTheDocument();
 });
 
-<<<<<<< HEAD
 test('renders Biology Today article', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Biology Today:/i);
-=======
+	render(<App />);
+	const linkElement = screen.getByText(/Biology Today:/i);
+	expect(linkElement).toBeInTheDocument();
+});
+
 test('renders Auto Review Article', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Auto Review:/i);
->>>>>>> feat: add Auto Review article
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/Auto Review:/i);
+	expect(linkElement).toBeInTheDocument();
 });
